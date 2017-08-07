64f5670d6538f2881524bc4503c927e200a1a8aa
zhengxiaoyao0716 <1499383852@qq.com>
2017-08-07 10:33:11 +0800

    改进example

--- example/zexample.go
+++ example/zexample.go
@@ -7,11 +7,25 @@ import (
 	"github.com/zhengxiaoyao0716/zmodule"
 )
 
+// In this way that override those values,
+// you can `main` as the module name, instead of `github.com/zhengxiaoyao0716/zmodule`.
+var (
+	Version   string // `git describe --tags`
+	Built     string // `date +%FT%T%z`
+	GitCommit string // `git rev-parse --short HEAD`
+	GoVersion string // `go version`
+)
+
 func main() {
 	zmodule.Author = "zhengxiaoyao0716"
 	zmodule.Homepage = "https://zhengxiaoyao0716.github.io/zmodule"
 	zmodule.Repository = "https://github.com/zhengxiaoyao0716/zmodule"
 	zmodule.License = "${Repository}/blob/master/LICENSE"
+
+	zmodule.Version = Version
+	zmodule.Built = Built
+	zmodule.GitCommit = GitCommit
+	zmodule.GoVersion = GoVersion
 
 	zmodule.Main("zexample",
 		&service.Config{
